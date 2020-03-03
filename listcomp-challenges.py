0357c4e31d7385a3137ba1c92a0e2c29ef62b8ff
bastosgabriel <bastos.gabriel@poli.ufrj.br>
2020-03-03 16:36:36 -0300

    Added list comprehensions little challenges

---
+++ listcomp-challenges.py
@@ -0,0 +1,23 @@
+# Challenge:
+
+# Use a dictionary comprehension to count the length of each word in a sentence.
+
+
+
+
+
+
+
+
+
+# Use a nested list comprehension to find all of the numbers from 1-1000 that are divisible by any single digit besides 1 (2-9)
+
+
+
+
+
+
+
+
+
+# For all the numbers 1-1000, use a nested list/dictionary comprehension to find the highest single digit any of the numbers is divisible by
