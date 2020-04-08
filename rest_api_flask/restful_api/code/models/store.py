ecc17d6562210cf2b25eab91cd262a5153f5c04e
bastosgabriel <bastos.gabriel@poli.ufrj.br>
2020-04-08 04:50:17 -0300

    Create store model

---
+++ rest_api_flask/restful_api/code/models/store.py
@@ -0,0 +1,37 @@
+import sqlite3
+from db import db
+
+class StoreModel(db.Model):
+    __tablename__ = 'stores'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+    items = db.relationship('ItemModel', lazy='dynamic')
+
+    def __init__(self, name, price):
+        self.name = name
+
+    '''
+    Returns a json formatted dictionary representing the store.
+    {'name': <itemname>, 'items': <items>}
+    '''
+    def json(self) -> dict:
+        return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    '''
+    Update or insert item model to .db file.
+    '''
+    def save_to_db(self) -> None:
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    Delete item model to .db file.
+    '''
+    def delete(self) -> None:
+        db.session.delete(self)
+        db.session.commit()
